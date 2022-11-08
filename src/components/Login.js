4ca73979ae356624ff5ca4f81423b5f7fd36afc0
joymush <joy.chemutai@student.moringaschool.com>
2022-11-08 21:50:14 +0300

    add use state effect

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function Login() {
-  return (
-    <div>Login</div>
-  )
+function Login({ setIsLoggedIn }) {
+  const history = useHistory();
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
 }
-
 export default Login
