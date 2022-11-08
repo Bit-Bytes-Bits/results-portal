3d6fdb19f24095c6de550e68c6526e1cc7a77dc4
joymush <joy.chemutai@student.moringaschool.com>
2022-11-09 01:37:19 +0300

    write function for user sign in and sign in failure

---
+++ src/components/loginform.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from "react";
+import GoogleLogin from "react-google-login";
+import { gapi } from "gapi-script";
+import "./loginform.css"
+
+const LoginForm = () => {
+
+    useEffect(() => {
+        function start() {
+            gapi.client.init({
+                clientId: "79474543031-tmjo35916ufn421ej3u1i2ljao2apr4s.apps.googleusercontent.com",
+                scope: ""
+            })
+        }
+        gapi.load('client: auth2', start)
+    })
+
+    const [popupStyle, showPopup] = useState("hide")
+
+    const popup = () => {
+        showPopup("login-popup")
+        setTimeout(() => showPopup("hide"), 3000)
+    }
+
+    const onSuccess = e => {
+        alert("User signed in")
+        console.log(e)
+    }
+
+    const onFailure = e => {
+        alert("User sign in Failed")
+        console.log(e)
+    }
+
+    return (
+        <div className="cover">
+            <h1>Login</h1>
+            
+        </div>
+    )
+}
