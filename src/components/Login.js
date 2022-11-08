684c826d635787b713adac20a262e123f2ae954f
joymush <joy.chemutai@student.moringaschool.com>
2022-11-09 02:02:51 +0300

    add a form

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,5 +19,26 @@ function Login({ setIsLoggedIn }) {
         setIsLoggedIn(true);
     }
 
-    ret
+    return (
+        <form className="form" onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <input
+               className="input"
+               type="text"
+               name="username"
+               value={FormData.username}
+               onChange={handleChange}
+            />
+            <input
+               className="input"
+               type="password"
+               name="password"
+               value={FormData.password}
+               onChange={handleChange}
+            />
+            <button type="submit">Login</button>
+        </form>
+    );
 }
+
+export default Login;
