5ed0774e95cde06980eb3953b9640af7b1d18c94
Gracelaura <gracenjuguna05@gmail.com>
2022-11-10 23:55:01 +0300

    added imports for react icons ,footer styling and formatted code to display the icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,38 @@
 import React from "react";
-import { FaPhoneSquareAlt , FaRegEnvelope} from "react-icons/fa";
-
+import { FaPhoneSquareAlt , FaRegEnvelope, FaInstagram, FaFacebookSquare, FaTwitterSquare} from "react-icons/fa";
+import "../styles/Footer.css"
 import "./App"
 
 function Footer () {
     return (
         <div className="footer">
-            <p>JAGIT UNIVERSITY 2022&copy;</p>
-            <div className="footer-content">
-                <FaPhoneSquareAlt className="contact-img"/>
-                 <p>070383999</p>
-                 <FaRegEnvelope className="email"/>
-                 <p>info@uni-jagit.com</p>
+            <div className="section1">
+                <h3>Contacts</h3>
+                <div className="footer-contact">
+                    <FaPhoneSquareAlt /> 0702233145<FaRegEnvelope /> 
+                </div>
             </div>
-           
+
+            <div className="section2">
+                <h3>Socials</h3>
+                <div className="socials">
+                    <FaInstagram /> <FaFacebookSquare/> <FaTwitterSquare />
+                </div>
+            </div>
+
+            <div className="section3">
+                <h2>Mission</h2>
+                <p>Our mission is to prepare creative and independent thinkers who are capable and confident global citizens. We provide a public service to our community and nation through generating, collating and preserving interdisciplinary academic knowledge.</p>
+            </div>
+
+            <div className="section4">
+                <h2>Vision</h2>
+                <p>Empowering our students to fulfill their academic and professional passions in a University that is diverse, welcoming, and inclusive for all students, faculty, and staff. Creating innovative connections among education, humanities, music, the social sciences, science, engineering, and health science.</p>
+            </div>
+            
         </div>
+           
+
     )
 }
 
