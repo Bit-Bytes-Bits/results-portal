8bb2e82d8ab822ceaca553bdf03049bf0b65395e
Gracelaura <gracenjuguna05@gmail.com>
2022-11-10 10:07:45 +0300

    added icons to the footer,added a footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { FaPhoneSquareAlt , FaRegEnvelope} from "react-icons/fa";
+
 import "./App"
 
 function Footer () {
     return (
         <div className="footer">
             <p>JAGIT UNIVERSITY 2022&copy;</p>
+            <div className="footer-content">
+                <FaPhoneSquareAlt className="contact-img"/>
+                 <p>070383999</p>
+                 <FaRegEnvelope className="email"/>
+                 <p>info@uni-jagit.com</p>
+            </div>
+           
         </div>
     )
 }
